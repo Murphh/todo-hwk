056d882dc97fed82e69064770ee41343fabd2e4f
Tech-a41 <tech-a41@Tech-A41s-MacBook-Air.local>
2019-04-17 09:18:15 +0100

    Added createP function to modularise code

--- js/app.js
+++ js/app.js
@@ -7,22 +7,20 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 
 function createToDo(toDo){
-  var today = new Date();
-  var contentArea = document.getElementsByClassName('main-content');
+  var contentSection = document.getElementById('main-content');
 
   var row = document.createElement('div');
   row.classList.add("todo-row");
-  var title = document.createElement("p");
-  title.innerHTML = toDo;
-  title.classList.add("todo-title");
-  row.appendChild(title);
+
+  row.appendChild(createP(["todo-title"], toDo));
   row.appendChild(createButton(["todo-button", "not-completed"], "Not Completed"));
-  var date = document.createElement('p');
-  date.classList.add('todo-date');
-  date.innerHTML = today.getDate() + "/" + (today.getMonth()+1) + "/" + today.getFullYear();
-  row.appendChild(date);
-  contentArea[0].insertBefore(row, contentArea[0].children[contentArea[0].children.length - 1]);
-  var lastToDoButton = contentArea[0].children[contentArea[0].children.length - 2].children[1];
+
+  var today = new Date();
+  var dateString = today.getDate() + "/" + (today.getMonth()+1) + "/" + today.getFullYear();
+  row.appendChild(createP(["todo-date"], dateString));
+
+  contentSection.insertBefore(row, contentSection.children[contentSection.children.length - 1]);
+  var lastToDoButton = contentSection.children[contentSection.children.length - 2].children[1];
 
   lastToDoButton.addEventListener('click', function(){
     this.classList.add("completed");
@@ -46,4 +44,13 @@ function createButton(classArr, buttonText){
   }
   button.innerText = buttonText;
   return button;
+}
+
+function createP(classArr, text){
+  var p = document.createElement("p");
+  for(var i=0; i < classArr.length ; i++){
+    p.classList.add(classArr[i]);
+  }
+  p.innerHTML = text;
+  return p;
 }
